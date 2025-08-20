429ab39d73ea033a488e6a35360f749d75156953
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-08-20 10:48:37 -0600

    Worked on advanced hello world

---
+++ practice/advanced_hw.py
@@ -0,0 +1,15 @@
+# LD 2nd Advanced Hello World
+
+admin = "Ms. LaRose"
+coder = "Liz"
+returning_users = []
+
+name = input("What is your name: ")
+
+if name == admin or name == coder:
+    print("Hello Admin", name)
+elif name == returning_users:
+    print("Hello", name)
+else:
+    print("Welcome", name)
+    returning_users.append(name)

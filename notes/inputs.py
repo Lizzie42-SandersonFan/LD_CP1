9c57e101886f871d6c5cf7190538fbcddbd2700a
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-08-20 10:24:30 -0600

    notes and hello world

---
+++ notes/inputs.py
@@ -0,0 +1,8 @@
+# LD 2nd Input/Output notes 
+
+color = input("Give me a color: ")
+print("Your color is", color)
+
+print("Tell me a color: ")
+color_two = input()
+print(color_two)
---
+++ practice/hello_world.py
@@ -0,0 +1,3 @@
+# LD, CP1 2nd, Hello World Program
+name = input("What is your name: ")
+print("Hello", name)

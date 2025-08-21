0d8a7f2f829197fb30f11b83180fd176aa003f0c
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-08-21 10:50:53 -0600

    Varaible notes

--- mastery_projects/who_are_you.py
+++ mastery_projects/who_are_you.py
@@ -4,4 +4,4 @@ name = input("What is your name: ")
 age = input("Great, how old are you: ")
 fav_color = input("Last one, what is your favorite color: ")
 
-print(name, "is", age, "old")
+print(name, "is", age, "years old and", fav_color, "is their favorite color!")

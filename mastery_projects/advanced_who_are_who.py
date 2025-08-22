7b9c7a896c0c259d1b8524a26a11a965a6870db5
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-08-22 10:50:05 -0600

    Worked on the advanced who are you

---
+++ mastery_projects/advanced_who_are_who.py
@@ -0,0 +1,8 @@
+# LD 2nd Advanced Who Are You
+
+# Have the program save each person's information, if a name is given for someone who has already been in the program then instead of asking the last two questions, tell them what they have already answered. (this program should just keep running)
+# Trying to use dictionaries for the repeat users
+
+x= 1
+while x == 1:
+    print("Hi")

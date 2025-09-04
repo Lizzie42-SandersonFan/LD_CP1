502a81e84e37dbf14ecc6f7dd0890250562879b2
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-09-04 02:03:09 +0000

    Made my MadLib EVEN MORE complicated

--- practice/madlib.py
+++ practice/madlib.py
@@ -4,10 +4,20 @@
 import time
 delay = 0.03
 
-
 # My functions - see line __ for start of running code
 def usersStoryChoice():
-    return story_choice == int(input("Would you like to do story one(1) or story two(2):\n"))
+    global story_choice
+    story_choice = int(input("Would you like to do story one(1) or story two(2):\n"))
+    if story_choice == 1:
+        storyOne()
+        stroyOneSentances()
+    elif story_choice == 2:
+        storyTwo()
+        storyTwoSentances()
+    else:
+        print("That is not a valid input, please try again.")
+        usersStoryChoice()
+
 
 def storyOne():
     # Variables in order based on story outline
@@ -34,6 +44,7 @@ def stroyOneSentances():
     global third_sentance
     global fourth_sentance
     global fifth_sentance
+    global story
 
     first_sentance = dragon_name + " the Dragon needed a new thing to colect after finishing his " + colection_noun_one + " colection."
 
@@ -45,9 +56,64 @@ def stroyOneSentances():
 
     fifth_sentance = dragon_name + " smiled at the idea of a " + silly_noun + " colection, and immediately got to work."
 
+    story = first_sentance + " " + second_sentance + " " + third_sentance + " " + fourth_sentance + " " + fifth_sentance + "\n"
+
+    for char in story:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+
 def storyTwo():
-    # Story outline: 
-    pass
+    # Story outline: On a [adjective] Halloween night, a [creature] crept out of the [place] holding a glowing [noun]. [Same creature] was searching for a [adjective] [object] said to be hidden behind the old [building]. Suddenly, a [same adjective] [same occupation] appeared, riding a flying [animal] and shouting [silly word]! They both screamed and ran through a forest full of [plural noun]. In the end, they shared a bag of [candy type] and promised to meet again next [holiday].
+    global adjective_one
+    global creature
+    global place
+    global noun_one
+    global adjective_two
+    global occupation
+    global building
+    global animal
+    global silly_word
+    global plural_noun
+    global candy_type
+    global holiday
+
+    adjective_one = input("\nGive me a adjective:\n").lower().strip()
+    creature = input("\nGive me a type of creature:\n").capitalize().strip()
+    place = input("\nGive me a place: \n").lower().strip()
+    noun_one = input("\nGive me a noun: \n").lower().strip()
+    adjective_two = input("\nGive me another adjective: \n").lower().strip()
+    occupation = input("\nGive me an occupation: \n").lower().strip()
+    building = input("\nGive me a building type: \n").lower().strip()
+    animal = input("\nGive me an animal: \n").lower().strip()
+    silly_word = input("\nGive me a silly word: \n").lower().strip()
+    plural_noun = input("\nGive me a plural noun: \n").lower().strip()
+    candy_type = input("\nGive me a type of candy: \n").lower().strip()
+    holiday = input("\nGive me a holiday: \n").capitalize().strip()
+
+def storyTwoSentances():
+    # Sentance variables concatinated to be printed later
+    global first_sentance
+    global second_sentance
+    global third_sentance
+    global fourth_sentance
+    global fifth_sentance
+    global story
+
+    first_sentance = "On a " + adjective_one + " Halloween night, a " + creature + " crept out of the " + place + " holding a glowing " + noun_one + "."
+
+    second_sentance = creature + " was searching for a(n) " + adjective_two + " " + occupation + " said to be hidden behind the old " + building + "."
+
+    third_sentance = "Suddenly, a " + adjective_two + " " + occupation + " appeared, riding a flying " + animal + " and shouting " + silly_word + "!"
+
+    fourth_sentance = "They both screamed and ran through a forest full of " + plural_noun + "."
+
+    fifth_sentance = "In the end, they shared a bag of " + candy_type + " and promised to meet again next " + holiday + "."
+
+    story = first_sentance + " " + second_sentance + " " + third_sentance + " " + fourth_sentance + " " + fifth_sentance + "\n"
+
+    for char in story:
+        print(char, end="", flush=True)
+        time.sleep(delay)
 
 # Welcome
 welcome_message = "Hello there! This a short Midlibs style game where you get to enter a word coresponding with the prompt.\n"
@@ -58,20 +124,11 @@ for char in welcome_message:
 story_choice = int(input("Would you like to do story one(1) or story two(2):\n"))
 
 if story_choice == 1:
-    print("Hello")
     storyOne()
+    stroyOneSentances()
 elif story_choice == 2:
-    print("Hi")
+    storyTwo()
+    storyTwoSentances()
 else:
     print("That is not a valid input, please try again.")
     usersStoryChoice()
-
-
-# All sentances concatinated
-story = first_sentance + " " + second_sentance + " " + third_sentance + " " + fourth_sentance + " " + fifth_sentance + "\n"
-
-
-# Printing the story
-for char in story:
-    print(char, end="", flush=True)
-    time.sleep(delay)

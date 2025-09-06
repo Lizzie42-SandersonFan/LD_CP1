cac014f1101e48321f6bbcac0a3698760080b970
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-09-06 00:27:02 +0000

    Finished idiot proof

--- practice/idiot_proof.py
+++ practice/idiot_proof.py
@@ -6,11 +6,16 @@ def phoneQuestionOne():
     global valid_one
     global length_one
 
-    phone_num_pt_one = input("What are the first three numbers of your phone number (Area code):\n")
+    phone_num_pt_one = input("\nWhat are the first three numbers of your phone number (Area code):\n")
     valid_one = phone_num_pt_one.isdigit()
-    #print(valid_one)
     length_one = len(phone_num_pt_one)
-    #print(length_one)
+
+    if valid_one == True and length_one == 3:
+        pass
+        # Passing because nothing needs to be done, the next question is called later 
+    else:
+        print("Sorry, that is not a valid input. Please try again.\n")
+        phoneQuestionOne()
 
 def phoneQuestionTwo():
     global phone_num_pt_two
@@ -19,9 +24,14 @@ def phoneQuestionTwo():
 
     phone_num_pt_two = input("\nWhat are the second three numbers of your phone number:\n")
     valid_two = phone_num_pt_two.isdigit()
-    #print(valid_two)
     length_two = len(phone_num_pt_two)
-    #print(length_two)
+
+    if valid_two == True and length_two == 3:
+        pass
+        # Passing because nothing needs to be done, the next question is called later
+    else:
+        print("Sorry, that is not a valid input. Please try again.\n")
+        phoneQuestionTwo()
 
 def phoneQuestionThree():
     global phone_num_pt_three
@@ -30,17 +40,31 @@ def phoneQuestionThree():
 
     phone_num_pt_three = input("\nWhat are the last four numbers of your phone number:\n")
     valid_three = phone_num_pt_three.isdigit()
-    #print(valid_three)
     length_three = len(phone_num_pt_three)
-    #print(length_three)
+
+    if valid_three == True and length_three == 4:
+        pass
+        # Passing because nothing needs to be done, the next question is called later
+    else:
+        print("Sorry, that is not a valid input. Please try again.\n")
+        phoneQuestionThree()
+
 
 def GPACheck():
     global gpa
     global rounded_gpa
+    global busted_gpa
 
-    gpa = float(input("\nWhat is (or was) your high school GPA (Grade Point Average):\n"))
+    gpa = input("\nWhat is (or was) your high school GPA (Grade Point Average):\n")
     busted_gpa = gpa.isalpha()
-    rounded_gpa = round(gpa, 1)
+
+    if busted_gpa == False:
+        float_gpa = float(gpa)
+        rounded_gpa = round(float_gpa, 1)
+    else:
+        print("That is not a valid input, please try again:\n")
+        GPACheck()
+    
 
 # Start of questions
 # Name question
@@ -48,7 +72,6 @@ name = input("What is your name:\n").strip().title()
 
 # Phone Question One
 phoneQuestionOne()
-# if checking if the input contains three numbers and is the correct length
 if valid_one == True and length_one == 3:
     pass
     # Passing because nothing needs to be done, the next question is called later 
@@ -58,7 +81,6 @@ else:
 
 # Phone Question Two
 phoneQuestionTwo()
-# if checking if the input contains three numbers and is the correct length
 if valid_two == True and length_two == 3:
     pass
     # Passing because nothing needs to be done, the next question is called later
@@ -68,7 +90,6 @@ else:
 
 # Phone Question Three
 phoneQuestionThree()
-# if checking if the input contains three numbers and is the correct length
 if valid_three == True and length_three == 4:
     pass
     # Passing because nothing needs to be done, the next question is called later
@@ -80,7 +101,12 @@ phone_number = phone_num_pt_one + " " + phone_num_pt_two + " " + phone_num_pt_th
 
 # GPA Question
 GPACheck()
-# IF TO SEE IF GPA IN NOT EQUAL TO ALPHA
+if busted_gpa == False:
+    float_gpa = float(gpa)
+    rounded_gpa = round(float_gpa, 1)
+else:
+    print("That is not a valid input, please try again:\n")
+    GPACheck()
 
 # Final Output
 print(f"Hello {name}. The phone number to reach you is {phone_number} and your high school GPA is/was {rounded_gpa}.")

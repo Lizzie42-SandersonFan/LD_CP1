115f9d182f784eff98d46d00e3ac09932073b8a6
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-09-09 10:49:24 -0600

    Finished my basic calculator

--- practice/basic_calculator.py
+++ practice/basic_calculator.py
@@ -1,25 +1,75 @@
 # LD 2nd Basic Calculator Practice
 
-# (Addition, Subtraction, Multiplication, Division, Integer Division and Modulo, and Exponents)
-# You will need 2 different inputs
-# Convert to correct data types to run equations
-# Your output needs to include the written equation and the result
-# All floats need to be rounded to 2 decimal places
-# Use a while loop to make the code run till the user chooses to leave (you will need a while loop).  Then let the user select which equation they would like to run instead of running all of them (You can do this with a conditional or with match)
-
-# Functions(?) for asking questions
+# Functions for asking questions
 def numOneAsk():
     global num_one
     num_one = int(input("Give me a WHOLE number:\n"))
 
 def numTwoAsk():
     global num_two
-    num_two = int(input("Give me another WHOLE number:\n"))
+    num_two = int(input("\nGive me another WHOLE number:\n"))
 
 def equationChoiceAsk():
     global equation_choice
-    equation_choice = input("Which equation would you like to do (Type corresponding number)\n1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Integer Division/Modulo 6) Exponants \n\n")
+    equation_choice = int(input("\nWhich equation would you like to do (Type corresponding number)\n1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Integer Division/Modulo\n6) Exponants \n\n"))
 
+    if equation_choice == equation_choice:
+        pass
+    else:
+        print("That is not a valid input, please try again.")
+        equationChoiceAsk()
+
+    if equation_choice == 1:
+        while equation_choice == 1:
+            print(f"{num_one} + {num_two} = {num_two + num_two}")
+            stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+            if stop == "YES":
+                break
+            else:
+                pass
+    elif equation_choice == 2:
+        while equation_choice == 2:
+            print(f"{num_one} - {num_two} = {num_two - num_two}")
+            stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+            if stop == "YES":
+                break
+            else:
+                pass
+    elif equation_choice == 3:
+        while equation_choice == 3:
+            print(f"{num_one} * {num_two} = {num_two * num_two}")
+            stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+            if stop == "YES":
+                break
+            else:
+                pass
+    elif equation_choice == 4:
+        while equation_choice == 4:
+            print(f"{num_one} / {num_two} = {num_two / num_two}")
+            stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+            if stop == "YES":
+                break
+            else:
+                pass
+    elif equation_choice == 5:
+        while equation_choice == 5:
+            print(f"{num_one} % {num_two} = {num_two % num_two}")
+            stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+            if stop == "YES":
+                break
+            else:
+                pass
+    elif equation_choice == 6:
+        while equation_choice == 6:
+            print(f"{num_one} ** {num_two} = {num_two ** num_two}")
+            stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+            if stop == "YES":
+                break
+            else:
+                pass
+    else:
+        print("That is not a valid input, please try again.")
+        equationChoiceAsk()
 
 # Ask questions first time
 numOneAsk()
@@ -36,4 +86,62 @@ if num_two == num_two:
 else:
     print("That is not a valid input, please try again.")
     numTwoAsk()
-# While loop to run the equations and periodicly ask if they want to stop
+
+if equation_choice == equation_choice:
+    pass
+else:
+    print("That is not a valid input, please try again.")
+    equationChoiceAsk()
+
+#Ifs that check the problem choice and then do a while loop
+if equation_choice == 1:
+    while equation_choice == 1:
+        print(f"{num_one} + {num_two} = {num_two + num_two}")
+        stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+        if stop == "YES":
+            break
+        else:
+            pass
+elif equation_choice == 2:
+    while equation_choice == 2:
+        print(f"{num_one} - {num_two} = {num_two - num_two}")
+        stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+        if stop == "YES":
+            break
+        else:
+            pass
+elif equation_choice == 3:
+    while equation_choice == 3:
+        print(f"{num_one} * {num_two} = {num_two * num_two}")
+        stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+        if stop == "YES":
+            break
+        else:
+            pass
+elif equation_choice == 4:
+    while equation_choice == 4:
+        print(f"{num_one} / {num_two} = {num_two / num_two}")
+        stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+        if stop == "YES":
+            break
+        else:
+            pass
+elif equation_choice == 5:
+    while equation_choice == 5:
+        print(f"{num_one} % {num_two} = {num_two % num_two}")
+        stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+        if stop == "YES":
+            break
+        else:
+            pass
+elif equation_choice == 6:
+    while equation_choice == 6:
+        print(f"{num_one} ** {num_two} = {num_two ** num_two}")
+        stop = input("\nWould you like to stop the code (YES or NO):\n").upper().strip()
+        if stop == "YES":
+            break
+        else:
+            pass
+else:
+    print("That is not a valid input, please try again.")
+    equationChoiceAsk()

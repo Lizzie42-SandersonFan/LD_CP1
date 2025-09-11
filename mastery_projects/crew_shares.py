b04abb2ef2caf7dc74eba977ddb17d90157c9685
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-09-11 14:59:51 +0000

    Finished Crew Shares

--- mastery_projects/crew_shares.py
+++ mastery_projects/crew_shares.py
@@ -2,40 +2,43 @@
 import time
 delay = 0.04
 
-# KEY REMINDERS:
-# You will need 2 different inputs (How much money earned and how many crew members there are)
-# Your program should calculate the base share, captain's share, first mate's share, and what the crew still need to be paid
-# The output has to change based on what the user has typed.
-# Money needs to be rounded to 2 decimals
-
-# INSTRUCTIONS
-# The crew earned a whole bunch of money on the last outing (an input in dollars), but the captain didn't have time to divvy it all up before release everyone to port. He gave each member of the crew 500 dollars for the evening and then sat down with his first mate to properly divide the shares. 
-# The captain of the crew gets 7 shares (equal portions of the treasure)
-# The first mate gets 3 shares
-# Each member of the crew then gets 1 share, but the crew members have all already received $500.
-# How much does the Captain get?  How much does the first mate get? How much needs to still be given to each member of the crew?
 
 # Welcom user and give background
-opening = "Hello! You are a captain on a ship and you and your crew just came back from an adventer looking for treasure.\nAfter your ship got to port, you didn't have enough time to calculate shares and just gave each of your crew members $500.\nThis progrm will calculate how much you, your first mate, and each crew member gets for you!\nBut first, some information is needed.\n"
+opening = "Hello! You are a captain on a ship and you and your crew just came back from an adventure looking for treasure.\nAfter your ship got to port, you didn't have enough time to calculate shares and just gave each of your crew members $500.\nThis program will calculate how much you, your first mate, and each crew member gets for you!\nBut first, some information is needed.\n"
 for char in opening:
     print(char, end="", flush=True)
     time.sleep(delay)
 
 # Get User inputs
-treasure_earned = float(input("\nHow much did you and your team find on the adventure:\n"))
+treasure_earned = float(input("\nHow much did you and your team find on the adventure(do Not include the $):\n"))
 treasure_money = round(treasure_earned, 2)
-crew_size = int(input("How many crew members are on your tem (Not Including You and the First Mate):\n"))
+crew_size = int(input("How many crew members are on your team (Not Including You and the First Mate):\n"))
 
 # Calculate base share
-
+base_share = treasure_money / (crew_size + 10)
+rouned_share = round(base_share, 2)
 
 # Get Captain's Share (Base * 7)
-
+captain_base = rouned_share * 7
+captains_share = round(captain_base, 2)
 
 # Get First Mate's Share (base * 3)
-
+first_mate_base = rouned_share * 3
+first_mates_share = round(first_mate_base, 2)
 
 # Get crew's share STILL NEEDED (base - 500)
-
+crew_base = rouned_share - 500
+crew_share_left = round(crew_base, 2)
 
 # Print Everything
+final_output = f"""
+Calculations have been run! Here are the final results:\n
+The money you earned was ${treasure_money:,}.
+There are {crew_size} people on your ship crew, not including you and your first mate.
+You, the captain, get ${captains_share:,} of the total ${treasure_money:,}.
+Your First Mate gets ${first_mates_share:,} of the total ${treasure_money:,}.
+Each member of your crew still needs ${crew_share_left:,} on top of their intial $500.
+"""
+for char in final_output:
+    print(char, end="", flush=True)
+    time.sleep(delay)

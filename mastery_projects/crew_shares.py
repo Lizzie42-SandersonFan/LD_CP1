5dfcf60ba890fd58d473fed9c34628ae7e074d70
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-09-15 10:30:12 -0600

    Did some grammer tweeks

--- mastery_projects/crew_shares.py
+++ mastery_projects/crew_shares.py
@@ -1,18 +1,23 @@
 # LD Crew Shares Mastery Project
 import time
-delay = 0.04
+delay = 0.06
 
 
 # Welcome user and give background
-opening = "Hello! You are a captain on a ship and you and your crew just came back from an adventure looking for treasure.\nAfter your ship got to port, you didn't have enough time to calculate shares and just gave each of your crew members $500.\nThis program will calculate how much you, your first mate, and each crew member gets for you!\nBut first, some information is needed.\n"
+opening = """Hello!
+You are a captain on a ship and you and your crew just came back from an adventure looking for treasure.
+After your ship got to port, you didn't have enough time to calculate shares and just gave each of your crew members $500.
+This program will calculate how much you, your first mate, and each crew member gets for you!
+But first, some information is needed.\n"""
+
 for char in opening:
     print(char, end="", flush=True)
     time.sleep(delay)
 
 # Get User inputs
-treasure_earned = float(input("\nHow much did you and your team find on the adventure(do Not include the $):\n"))
+treasure_earned = float(input("\nHow much did you and your team find on the adventure(Do NOT include the $ OR ,):\n"))
 treasure_money = round(treasure_earned, 2)
-crew_size = int(input("How many crew members are on your team (Not Including You and the First Mate):\n"))
+crew_size = int(input("How many crew members are on your team (NOT Including You and the First Mate):\n"))
 
 # Calculate base share
 base_share = treasure_money / (crew_size + 10)
@@ -35,7 +40,7 @@ final_output = f"""
 Calculations have been run! Here are the final results:\n
 The money you earned was ${treasure_money:,}.
 There are {crew_size} people on your ship crew, not including you and your first mate.
-You, the captain, get ${captains_share:,} of the total ${treasure_money:,}.
+You, the captain, gets ${captains_share:,} of the total ${treasure_money:,}.
 Your First Mate gets ${first_mates_share:,} of the total ${treasure_money:,}.
 Each member of your crew still needs ${crew_share_left:,} on top of their intial $500.
 """

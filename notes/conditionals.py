fc32744de7b9a96bbd8eacd4a0afff09834c8488
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-09-19 10:42:15 -0600

    conditional notes

---
+++ notes/conditionals.py
@@ -0,0 +1,36 @@
+# LD 2nd Conditionals Notes
+import random
+monster_hp = 30
+dmg_modifier = 2
+atk_bonus = 3
+player_hp = 25
+
+roll = random.randint(1,20)
+
+if roll == 20:
+    print(f"You rolled a crit! Double you damage.")
+    attack = random.randint(1,8) + random.randint(1,8) + dmg_modifier
+    monster_hp -= attack
+    print(f"You did {attack} damage to the monster!")
+elif roll+atk_bonus > 10:
+    print(f"You hit!")
+    attack = random.randint(1,8) + dmg_modifier
+    monster_hp -= attack
+    print(f"You did {attack} damage to the monster!")
+elif roll <= 10:
+    if roll == 1:
+        print(f"You rolled critical failure! The monster gets a free attack!")
+        damage = random.randint(1,10) + 2
+        player_hp -= damage
+        print(f"You took {damage} damage. You know have {player_hp} HP.")
+    else:
+        print(f"You missed!")
+else:
+    print(f"That shouldn't be possible")
+
+print("Your turn is over.")
+
+if monster_hp and monster_hp > 0:
+    print("It is the monster's turn")
+else:
+    print("The monster is dead")   

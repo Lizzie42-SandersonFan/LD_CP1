93a2757fbc44b8602c769a4804c0ff87538d8145
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-10-20 10:50:56 -0600

    Notes done

---
+++ notes/functions.py
@@ -0,0 +1,14 @@
+# LD 2nd Functions Notes
+
+num = 0
+def add(x , y):
+    return x+y
+
+while num < add(5, 5):
+    print("Duck")
+    num+=1
+print("Goose")
+print(f" Reswults is: {add(8746, 86135)}")
+total =add(.57235, .4145658)
+print(add(add(-436, 24643), 10))
+add(42, 7)

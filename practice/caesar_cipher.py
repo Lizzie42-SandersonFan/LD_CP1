9ec7c9786b80c4f4c567822ed130220f8e0d6ebc
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-10-23 01:50:11 +0000

    Finished caeser cipher

--- practice/caesar_cipher.py
+++ practice/caesar_cipher.py
@@ -3,47 +3,52 @@
 # Any variables eventually needed. Messsage and shift value
 message = ""
 shift_value = 0
+lower_a = 97
+lower_z = 122
+upper_A = 65
+upper_Z = 90
 
 # Functions
 # Funtion that will encode using the message and shift value. use the ord() function to get each cahracter's value then add the shift value. Use chr() to get the new letter replacement, build all results into a string then RETURN final value
 # Uppercase is 65-90
 # Lowercase is 97-122
 def encode(msg, shift):
-    values = []
+    shift = shift % 26
     new_message = ""
     for char in msg:
-        x = ord(char) + shift
-        # To deal with spaces, check to see if it was origanally a space and kepp if that
-        if x > 90 and (char.isupper() == True):
-            x -= 26
-        elif x > 122 and (char.islower() == True):
-            x -= 26
+        if ord(char) >= lower_a and ord(char) <= lower_z:
+            # 1 add the shift
+            shifted_letter = ord(char) + shift
+            # 2 make sure it stays lowercase
+            if shifted_letter < lower_a:
+                shifted_letter += 26
+            elif shifted_letter > lower_z:
+                shifted_letter -= 26
+            else:
+                pass # It's already in the right spot
+            # 3 put it into the final string
+            new_message += chr(shifted_letter)
+        elif ord(char) >= upper_A and ord(char) <= upper_Z:
+            # 1 add the shift
+            shifted_letter = ord(char) + shift
+            # 2 make sure it stays uppercase
+            if shifted_letter < upper_A:
+                shifted_letter += 26
+            elif shifted_letter > upper_Z:
+                shifted_letter -= 26
+            else:
+                pass # It's already in the right spot
+            #3 put it into the final string
+            new_message += chr(shifted_letter)
         else:
-            pass # passing because I don't know what could go in here and x's new value is established
-        values.append(x)
-    for value in values:
-        y = chr(value)
-        new_message += y
+            # put into final string
+            new_message += char
     return new_message
 
 
-# Funtion that will decode using the message and shift value. use the chr() function to get each cahracter's value then subrtact the shift value.Use chr() to get the new letter replacement (should be the original message), build all results into a string then RETURN final value
+# Funtion that will decode using the message and shift value. Encode but backwards.
 def decode(msg, shift):
-    values = []
-    og_message = ""
-    for char in msg:
-        x = ord(char) - shift
-        if x > 90 and (char.isupper() == True):
-            x -= 26
-        elif x > 122 and (char.islower() == True):
-            x -= 26
-        else:
-            pass # passing because I don't know what could go in here and x's new value is established
-        values.append(x)
-    for value in values:
-        y = chr(value)
-        og_message += y
-    return og_message
+    return encode(msg, 26-shift)
 
 # Begin code
 # Use input() to get the stuff listed above, then ask the user if they want to encode or decode. Call corresponding function based on choice.

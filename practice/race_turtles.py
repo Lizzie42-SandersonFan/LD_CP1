5ecf6eaf4bd8c821cee32fea86669718534d6d75
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-10-28 10:50:53 -0600

    pseudo code

--- practice/race_turtles.py
+++ practice/race_turtles.py
@@ -1,2 +1,11 @@
 # LD 2nd Race Turtles
+# Import libraries
 
+# Set up visuals first:
+    # Make finish line. set as a variable?
+
+    # Make FIVE different colored turtles and establish a random speed for each
+
+# Do python code part:
+    # Check for when a turtle crosses the finish line
+        # if a turtle does, stop the game and show which turtle color won the race

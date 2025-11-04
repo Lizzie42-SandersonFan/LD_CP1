297a9b8d6a4ad3a4130bed37a5e0a8c205515b96
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-11-04 10:51:34 -0700

    I need to fix stuff

--- practice/maze_generator.py
+++ practice/maze_generator.py
@@ -7,20 +7,11 @@ import random
 pen = turtle.Turtle()
 pen.hideturtle()
 
-rows = [
-    [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)]
-]
-columns = [
-    [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)]
-]
-
-# Randomly generate maze from lists
-
 # Function to check if maze is solvable
 def isSolvable(row_grid, col_grid):
     size = len(row_grid) - 1
     visited = set()
-    stack = [(0,0)]
+    stack = [(0s,0)]
 
     while stack:
         x, y = stack.pop()
@@ -47,39 +38,52 @@ def isSolvable(row_grid, col_grid):
 
     return False
 
-# For loop to draw the maze itself. For item in maze, then for border in item: draw. If border is true(1), then pendown and draw then penup for every item. False(0) dont draw
-# Set possition for drawing rows \/
-pen.penup()
-pen.setpos(-90, 90)
-pen.pendown()
+while True:
+    rows = [
+        [0, 1, 1, 1, 1, 1], [random.randint(0,2), random.randint(0,2), 0, random.randint(0,2), 0, random.randint(0,2)], [random.randint(0,2), random.randint(0,2), 1, random.randint(0,2), 1, random.randint(0,2)], [random.randint(0,2), 1, random.randint(0,2), random.randint(0,2), 0, random.randint(0,2)], [random.randint(0,2), random.randint(0,2), 0, random.randint(0,2), random.randint(0,2), 1], [random.randint(0,2), 1, random.randint(0,2), 1, random.randint(0,2), random.randint(0,2)], [1, 1, 1, 1, 1, 0]
+    ]
+    columns = [
+        [1, 1, 1, 1, 1, 1], [random.randint(0,2), 1, random.randint(0,2), random.randint(0,2), 0, random.randint(0,2)], [random.randint(0,2), 0, 1, random.randint(0,2), random.randint(0,2), random.randint(0,2)], [1, random.randint(0,2), random.randint(0,2), 0, random.randint(0,2), random.randint(0,2)], [random.randint(0,2), 0, random.randint(0,2), 1, random.randint(0,2), random.randint(0,2)], [1, random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), 0], [1, 1, 1, 1, 1, 1]
+    ]
 
-for row in rows:
-    for section in row:
-        if section == 0:
+    # Randomly generate maze from lists
+    # For loop to draw the maze itself. For item in maze, then for border in item: draw. If border is true(1), then pendown and draw then penup for every item. False(0) dont draw
+    if isSolvable(rows, columns) == True:
+        # Set possition for drawing rows \/
+        pen.penup()
+        pen.setpos(-90, 90)
+        pen.pendown()
+
+        for row in rows:
+            for section in row:
+                if section == 0:
+                    pen.penup()
+                    pen.forward(30)
+                else:
+                    pen.pendown()
+                    pen.forward(30)
             pen.penup()
-            pen.forward(30)
-        else:
+            pen.setpos(-90, pen.ycor()-30)
             pen.pendown()
-            pen.forward(30)
-            pen.penup()
 
-# Set possition for drawing columns \/
-pen.penup()
-pen.right(90)
-pen.setpos(-90, 90)
-pen.pendown()
+        # Set possition for drawing columns \/
+        pen.penup()
+        pen.right(90)
+        pen.setpos(-90, 90)
+        pen.pendown()
 
-for column in columns:
-    for wall in column:
-        if wall == 0:
+        for column in columns:
+            for wall in column:
+                if wall == 0:
+                    pen.penup()
+                    pen.forward(30)
+                else:
+                    pen.pendown()
+                    pen.forward(30)
             pen.penup()
-            pen.forward(30)
-        else:
+            pen.setpos(pen.xcor()+30, 90)
             pen.pendown()
-            pen.forward(30)
-            pen.penup()
+    else:
+        continue
 
-# true_maze = isSolvable(rows, columns)
-# print(true_maze)
-
-turtle.done()
+    turtle.done()

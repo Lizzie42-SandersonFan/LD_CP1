d033183e910690f87a8d077809e9e2920dd1bfdb
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-11-05 10:51:05 -0700

    Almost there

--- practice/maze_generator.py
+++ practice/maze_generator.py
@@ -10,8 +10,8 @@ pen.hideturtle()
 # Function to check if maze is solvable
 def isSolvable(row_grid, col_grid):
     size = len(row_grid) - 1
-    visited = set()
-    stack = [(-90,90)]
+    visited = []
+    stack = [(0,0)]
 
     while stack:
         x, y = stack.pop()
@@ -22,20 +22,22 @@ def isSolvable(row_grid, col_grid):
         if (x, y) in visited:
             continue
 
-        visited.add((x, y))
-
-        if x < size - 1 and col_grid[x+1][y] == 0:
-            stack.append((x+1, y))
-
-        if y < size - 1 and row_grid[x][y+1] == 0:
-            stack.append((x, y+1))
-
-        if x > 0 and col_grid[x-1][y] == 0:
+        visited.append((x, y))
+        # If row below me is there
+        if x <= size - 1 and col_grid[y][x-1] == 0:
             stack.append((x-1, y))
 
-        if y > 0 and row_grid[x][y-1] == 0:
+        # If column to left is there
+        if y <= size - 1 and row_grid[x][y-1] == 0:
             stack.append((x, y-1))
 
+        # If row above is there
+        if x >= 0 and col_grid[x][y] == 0:
+            stack.append((x, y+1))
+
+        # If column to right is there
+        if y >= 0 and row_grid[x][y] == 0:
+            stack.append((x+1, y))
     return False
 
 # While loop to keep generating the random maze until one is solvable, then show the solvable maze
@@ -43,18 +45,14 @@ while True:
     # Set up grid, this is two lists. one for rows and other for columns
     # Randomly generate maze from lists
     rows = [
-        [0, 1, 1, 1, 1, 1], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [1, 1, 1, 1, 1, 0]
+        [0, 1, 1, 1, 1, 1], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [1, 1, 1, 1, 1, 0]
     ]
     columns = [
-        [1, 1, 1, 1, 1, 1], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2), random.randint(0,2)], [1, 1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)], [1, 1, 1, 1, 1, 1]
     ]
     
     # Use function to check if the generated maze is solvable; if it is, show maze. If not, go back and re-randamize the maze
     if isSolvable(rows, columns) == True:
-        # Set possition for drawing rows \/
-        pen.penup()
-        pen.setpos(-90, 90)
-        pen.pendown()
         # For loop to draw the maze itself. For item in maze, then for border in item: draw. If border is true(1), then pendown and draw then penup for every item. False(0) dont draw
         for row in rows:
             for section in row:
@@ -65,15 +63,13 @@ while True:
                     pen.pendown()
                     pen.forward(30)
             pen.penup()
-            pen.setpos(-90, pen.ycor()-30)
+            pen.setpos(0, pen.ycor()-30)
             pen.pendown()
-
-        # Set possition for drawing columns \/
+        
+        # Reset turtle for columns
         pen.penup()
+        pen.setpos(0,0)
         pen.right(90)
-        pen.setpos(-90, 90)
-        pen.pendown()
-
         for column in columns:
             for wall in column:
                 if wall == 0:
@@ -83,9 +79,9 @@ while True:
                     pen.pendown()
                     pen.forward(30)
             pen.penup()
-            pen.setpos(pen.xcor()+30, 90)
+            pen.setpos(pen.xcor()+30, 0)
             pen.pendown()
+        turtle.done()
+        break
     else:
         continue
-
-    turtle.done()

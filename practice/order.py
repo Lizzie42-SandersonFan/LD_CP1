16e9a89798a2e5bc4195acae5d3e8fd8c9073e54
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-11-10 10:50:55 -0700

    Started Menu dictionary

---
+++ practice/order.py
@@ -0,0 +1,19 @@
+# LD 2nd Order Up Practice
+
+# Build a dictionary with menu items
+menu = {
+    "cookies_one": ["Milk Chocolate Chip", "Dirt Cake", "Raspberry Lemonade", "S'mores", "Chocolate Cake Batter", "Lemon Bar", "Birthday Cake"],
+    "cookies_two": [],
+    "cookies_three": [],
+    "cookies_four": [],
+    "cookies_five": [],
+    "cookies_six": []
+}
+
+# Dictionary that is the item selected and its price
+# This dictionary will slowly be built up through if statements to see what the user ordered
+
+# If statements that will ask the user what they want based on the menu dictionary
+# Should do some while loops to asked again if option is invalid
+
+# Print out order and final price

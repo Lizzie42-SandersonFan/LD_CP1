c80f664149fb1f98a16d85e5b22633b2154c45e4
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-11-17 10:50:33 -0700

    started on flexible calculator

---
+++ practice/flexible_calculator.py
@@ -0,0 +1,33 @@
+# LD 2nd Flexible Calculator
+import statistics
+
+users_nums = []
+# Build function for sum
+def sum(*nums):
+    pass
+
+# Build function for average
+def avrg(*nums):
+    pass
+
+# Build function for min
+def min(*nums):
+    pass
+
+# Build function for max
+def max(*nums):
+    pass
+
+# Build function for product
+def multiply(*nums):
+    pass
+
+
+# Big while loop for running game
+while True:
+    # While loop for gettign user's operation choice
+    pass
+
+    # While loop for getting list of numbers
+
+# When user is done, thank them

d261ec9600954fd1bb12e11a078b04e14bf7f408
Lizzie42-SandersonFan <lizzie.delong@ucas-edu.net>
2025-11-21 10:50:44 -0700

    factorial practice

---
+++ practice/factorial_code.py
@@ -0,0 +1,50 @@
+# LD 2nd Factorial based on Elbow Partner's pseudocode
+
+#num_list = []
+num_list = []
+
+# Define factorial(number, num_list):
+#append number to num_list
+# while num_list >1:
+# factored_number = number - 1
+# factored_number append to num_list
+
+#define print_factoral(number, factored_number, num_list):
+# multiply all numbers in num_list and save as factored number
+
+def factorial(number, num_list):
+    num_list.append(number)
+    while number >= 1:
+        number -= 1
+        num_list.append(number)
+
+
+def print_factorial(number, num_list):
+    factored_number = 1
+    for num in num_list:
+        factored_number *= num
+    return factored_number
+
+# while true:
+# num = input("Enter a whole factorable number:")
+# if num.isdiget == True:
+# factoral(num, num_list)
+# elif num.isdiget() == False:
+# display: Invalid input, please retry with a whole number
+# contiue
+while True:
+    num = int(input("Enter a whole, factorable number:\n"))
+    if num.isnumeric() == True:
+        factorial(num, num_list)
+        break
+    elif num.isdigit() == False:
+        print("Invalid input, please retry with a whole number")
+        continue
+
+factored_num = print_factorial(num, num_list)
+
+# LD notes:
+# need to break in while loop
+# print funtion doesn't need factor_number paramiter
+# in factorial funtion, do not need to compare list, but the number, and also just do number and not all this factored_num thing
+# isdigit isn't working
